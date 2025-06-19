136301cc2faecec9760c5280b0439bf4c32bb22d
Angel Jaret Lopez Hernandez <jaretguetta@icloud.com>
2025-06-19 00:11:36 -0600

    Intento 3 Static

--- atizacitas/settings.py
+++ atizacitas/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostaticcitas'
+    'whitenoise.runserver_nostatic',
     'citas',
 ]
 
